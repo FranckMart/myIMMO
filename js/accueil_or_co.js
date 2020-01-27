5480100d7cc8ea4881f95fc440502df08c876f43
DEJEAN Hugo <h.dejean@ics-nice.com>
2020-01-27 16:25:08 +0100

    last hugo

--- js/accueil_or_co.js
+++ js/accueil_or_co.js
@@ -1,22 +1,22 @@
-jQuery(document).ready(function ($) {
+
 
    
-$(window).load(function() {
+$(window).on("load",function() {
     var slideCount = $('#slider ul li').length;
-	var slideWidth = $('#slider ul li').width();
-	var slideHeight = $('#slider ul li').height();
+	var slideWidth = $('#slider img').width();
+	var slideHeight = $('#slider img').height();
 	var sliderUlWidth = slideCount * slideWidth;
 	
 	$('#slider').css({ width: slideWidth, height: slideHeight });
 	
-	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+	//$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
 	
     $('#slider ul li:last-child').prependTo('#slider ul');
 
     function moveLeft() {
         $('#slider ul').animate({
             left: + slideWidth
-        }, 200, function () {
+        }, 400, function () {
             $('#slider ul li:last-child').prependTo('#slider ul');
             $('#slider ul').css('left', '');
         });
@@ -25,17 +25,16 @@ $(window).load(function() {
     function moveRight() {
         $('#slider ul').animate({
             left: - slideWidth
-        }, 200, function () {
+        }, 400, function () {
             $('#slider ul li:first-child').appendTo('#slider ul');
             $('#slider ul').css('left', '');
         });
     };
-
-});    
-
     setInterval(function () {
         moveRight();
     }, 3000);
   });
+   
+
   
 	
