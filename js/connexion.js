69e8fdffaa47c6e37acd66a0ab3fcb31db8547ec
Franck Martinez <franckmartinez22@gmail.com>
2020-01-28 12:24:26 +0100

    Regex pseudo et mdp

---
+++ js/connexion.js
@@ -0,0 +1,28 @@
+//var pseudoAide = document.getElementById("");
+var formElt = document.getElementById("sub-form");
+var pseudoAide = document.createElement("p");
+pseudoAide.textContent = "Pseudo doit avoir entre 6 et 12 lettres ou 6 à 12 letrres et 2 chiffres";
+pseudoAide.style.color = "red";
+pseudoAide.style.margin = "10px";
+
+formElt.addEventListener("submit", function (e) {
+    e.preventDefault()
+
+    var pseudo = formElt.elements.pseudo.value;
+    var mdp = formElt.elements.pwd.value;
+    var regexPseudoNan = /^[a-zA-Z0-9_-]{6,12}$/;
+    var regexPseudoNb = /^(?=.*[a-zA-Z0-9_-])(?=.*[0-9]){6,14}$/;
+   // var regexMdp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$/
+    var regexMdp = /^(?=.{10,}$)(?=.?[a-z])(?=.?[A-Z])(?=.?[0-9])(?=.?\W).*$/
+
+    if ((!regexPseudoNan.test(pseudo)) || (regexPseudoNb.test(pseudo))){
+        alert("Pseudo Erreur");
+        document.getElementById("inscription").appendChild(pseudoAide);
+    }else if((!regexMdp.test(mdp))) {
+        alert("Mdp Erreur");
+    }
+    
+    formElt.reset()
+
+
+})
