0140e0adb4da2cb20eef188608177b0292da079c
Franck Martinez <franckmartinez22@gmail.com>
2020-01-29 13:30:19 +0100

    Modification header et responsive

--- js/inscription.js
+++ js/inscription.js
@@ -6,6 +6,6 @@ function regmaster(params) {
     // reg mdp (Un mot de passe contenant au moins 1 majuscule, 1 minuscule, 1 chiffre, 1 caractère spécial et une longueur d'au moins 10)
         var regexmdp = /^(?=.{10,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/
     // reg pseudo
-        var regexpseudo =
+        var regexpseudo;
 
 }
